b22ca2ecb4b77cdc8f28ffadbd29a3a1f8b351d5
Christian Schmidt <chris@skulltree.de>
2025-11-20 23:49:54 +0100

    Declare Mod_BspxDebugf prototype

--- Quake/gl_model.c
+++ Quake/gl_model.c
@@ -38,6 +38,7 @@ static void Mod_LoadMD2Model (qmodel_t *mod, void *buffer);
 static void Mod_LoadMD5MeshModel (qmodel_t *mod, const char *buffer);
 static qmodel_t *Mod_LoadModel (qmodel_t *mod, qboolean crash);
 static void Mod_LoadBspx (const byte *buffer);
+static void Mod_BspxDebugf (const char *fmt, ...);
 
 static void Mod_Print (void);
 
@@ -53,6 +54,22 @@ static int	mod_novis_capacity;
 static byte	*mod_decompressed;
 static int	mod_decompressed_capacity;
 static size_t   mod_bspx_filesize;
+
+static const byte *Mod_BspxCopyLump (const char *lumpname, const byte *data, size_t length)
+{
+        if (!data || length == 0)
+                return NULL;
+
+	byte *copy = (byte *)Hunk_AllocName (length, "BSPXDATA");
+	if (!copy)
+	{
+		Mod_BspxDebugf ("BSPX:     %s failed to allocate %zu bytes\n", lumpname, length);
+		return NULL;
+	}
+
+	memcpy (copy, data, length);
+	return copy;
+}
 
 static size_t Mod_BspLumpElementSize (int lump_index, int bsp2)
 {
@@ -1859,8 +1876,13 @@ static void Mod_LoadBspx (const byte *buffer)
                         lumpofs = rel_ofs;
                 }
 
+                const byte *lumpdata = lumplen ? Mod_BspxCopyLump (lumpname, buffer + lumpofs, lumplen) : NULL;
+
+                if (lumplen && !lumpdata)
+                        continue;
+
                 q_strlcpy (loadmodel->bspx_entries[stored].name, lumpname, sizeof(loadmodel->bspx_entries[stored].name));
-                loadmodel->bspx_entries[stored].data = buffer + lumpofs;
+                loadmodel->bspx_entries[stored].data = lumpdata;
                 loadmodel->bspx_entries[stored].length = lumplen;
 
                 if (!q_strcasecmp (lumpname, "RGBLIGHTING") ||
