b05ea988a053a4a6d1d79fab0fd9a2252333c309
Christian Schmidt <chris@skulltree.de>
2025-12-15 01:38:40 +0100

    Use deluxemap directions in lightgrid generation

--- Quake/glquake.h
+++ Quake/glquake.h
@@ -519,6 +519,7 @@ void GLMesh_DeleteVertexBuffers (void);
 
 int R_LightPoint (qmodel_t *model, vec3_t p, float ofs, lightcache_t *cache);
 qboolean R_SampleLightmapAtPoint(const vec3_t pos, vec3_t out_rgb);
+qboolean R_SampleLightmapAndDeluxemapAtPoint(const vec3_t pos, vec3_t out_rgb, vec3_t out_dir);
 qboolean R_LightgridEnabled (void);
 void R_LightgridLighting (const vec3_t pos, vec3_t out_color);
 void R_LightgridLightingDir (const vec3_t pos, vec3_t out_color, vec3_t out_dir);
