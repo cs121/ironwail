7beda9f8f397facefd082d27bd3cbe5b9d306426
Christian Schmidt <chris@skulltree.de>
2026-01-22 20:33:25 +0100

    Include net_defs for net debug fields

--- Quake/cl_input.c
+++ Quake/cl_input.c
@@ -25,6 +25,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 // rights reserved.
 
 #include "quakedef.h"
+#include "net_defs.h"
 
 extern cvar_t cl_maxpitch; //johnfitz -- variable pitch clamping
 extern cvar_t cl_minpitch; //johnfitz -- variable pitch clamping
