f605c659103f50ed6824a0d061ee727ac996ba9f
Christian Schmidt <chris@skulltree.de>
2026-02-18 18:33:28 +0100

    Fix MAX_SFX visibility for sound memory module

--- Quake/q_sound.h
+++ Quake/q_sound.h
@@ -155,6 +155,7 @@ void SNDDMA_UnblockSound(void);
  * ====================================================================
  */
 
+#define	MAX_SFX			1024
 #define	MAX_CHANNELS		1024 // ericw -- was 512 /* johnfitz -- was 128 */
 #define	MAX_DYNAMIC_CHANNELS	128 /* johnfitz -- was 8   */
 
