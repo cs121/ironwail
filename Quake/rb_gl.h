e35f312fc3016896e1d1183f93b4b770f1cf4dde
Christian Schmidt <chris@skulltree.de>
2026-02-27 13:01:04 +0100

    rb_gl: track owners for FBO/VAO/buffer/sampler binds

--- Quake/rb_gl.h
+++ Quake/rb_gl.h
@@ -14,13 +14,24 @@
 void RB_SetState_Owner (unsigned mask, const char *owner);
 void RB_UseProgram_Owner (GLuint program, const char *owner);
 qboolean RB_BindTexture_Owner (GLenum texunit, gltexture_t *texture, const char *owner);
+void RB_BindFramebuffer_Owner (GLenum target, GLuint framebuffer, const char *owner);
+void RB_BindVertexArray_Owner (GLuint array, const char *owner);
+void RB_BindBuffer_Owner (GLenum target, GLuint buffer, const char *owner);
+void RB_BindSampler_Owner (GLuint unit, GLuint sampler, const char *owner);
 #define RB_SetState(mask) RB_SetState_Owner ((mask), __func__)
 #define RB_UseProgram(program) RB_UseProgram_Owner ((program), __func__)
 #define RB_BindTexture(texunit, texture) RB_BindTexture_Owner ((texunit), (texture), __func__)
+#define RB_BindFramebuffer(target, framebuffer) RB_BindFramebuffer_Owner ((target), (framebuffer), __func__)
+#define RB_BindVertexArray(array) RB_BindVertexArray_Owner ((array), __func__)
+#define RB_BindBuffer(target, buffer) RB_BindBuffer_Owner ((target), (buffer), __func__)
+#define RB_BindSampler(unit, sampler) RB_BindSampler_Owner ((unit), (sampler), __func__)
 #define RB_SetStateWithOwner(mask, owner) RB_SetState_Owner ((mask), (owner))
 #define RB_UseProgramWithOwner(program, owner) RB_UseProgram_Owner ((program), (owner))
 #define RB_BindTextureWithOwner(texunit, texture, owner) RB_BindTexture_Owner ((texunit), (texture), (owner))
-void RB_BindFramebuffer (GLenum target, GLuint framebuffer);
+#define RB_BindFramebufferWithOwner(target, framebuffer, owner) RB_BindFramebuffer_Owner ((target), (framebuffer), (owner))
+#define RB_BindVertexArrayWithOwner(array, owner) RB_BindVertexArray_Owner ((array), (owner))
+#define RB_BindBufferWithOwner(target, buffer, owner) RB_BindBuffer_Owner ((target), (buffer), (owner))
+#define RB_BindSamplerWithOwner(unit, sampler, owner) RB_BindSampler_Owner ((unit), (sampler), (owner))
 void RB_DrawArrays (GLenum mode, GLint first, GLsizei count);
 void RB_DrawElements (GLenum mode, GLsizei count, GLenum type, const GLvoid *indices);
 void RB_Clear (GLbitfield mask);
