6c93442880932905ecbd3bcda8e9e8d7058be51b
Christian Schmidt <cs@cw-it.de>
2026-04-16 16:32:00 +0200

    fix: forward botspawn from console

--- Quake/host_cmd.c
+++ Quake/host_cmd.c
@@ -3153,6 +3153,12 @@ static void Host_BotSpawn_f (void)
 	dfunction_t *func;
 	edict_t *self;
 
+	if (cmd_source == src_command)
+	{
+		Cmd_ForwardToServer ();
+		return;
+	}
+
 	if (!sv.active || sv.state != ss_active)
 	{
 		Con_Printf ("botspawn is only available while a server is active\n");
@@ -3166,8 +3172,14 @@ static void Host_BotSpawn_f (void)
 		return;
 	}
 
+	if (!sv_player)
+	{
+		Con_Printf ("botspawn requires an active player\n");
+		return;
+	}
+
 	pr_global_struct->time = qcvm->time;
-	self = (cmd_source == src_command) ? qcvm->edicts : sv_player;
+	self = sv_player;
 	pr_global_struct->self = EDICT_TO_PROG(self);
 
 	PR_ExecuteProgram (func - qcvm->functions);
